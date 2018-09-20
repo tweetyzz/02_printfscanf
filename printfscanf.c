e7bfd82904e43b2eb46dab850437985f4be5c837
tweety <ekak9380@gmail.com>
2018-09-20 16:30:37 +0900

    02_change

--- printfscanf.c
+++ printfscanf.c
@@ -4,16 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int inInt;
-	float inFloat;
+	char c;
+	int i;
 	
-	printf("input an integer : ");
-	scanf("%d",&inInt);
+	printf("input a number : ");
+	scanf("%c,&c");
 	
-	printf("input an float :");
-	scanf("%d",&inFloat);
-	
-	printf("integer : %i, float : %f\n", inInt, inFloat);
-	
+	i= c - '0';
+	printf("The input number is %i\n",i);
 	return 0;
+
 }
