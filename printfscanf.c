199bd0b0fed8bee3642649607abf678c7145bc97
tweety <ekak9380@gmail.com>
2018-09-20 16:51:51 +0900

    03 changemaster

--- printfscanf.c
+++ printfscanf.c
@@ -5,13 +5,13 @@
 
 int main(int argc, char *argv[]) {
 	char c;
-	int i;
 	
-	printf("input a number : ");
-	scanf("%c,&c");
 	
-	i= c - '0';
-	printf("The input number is %i\n",i);
+	printf("input a character : ");
+	scanf("%c",&c);
+	
+	printf("The next character of %c (%i) is %c (%i)\n",c ,c ,c+1, c+1);
+	
 	return 0;
 
 }
